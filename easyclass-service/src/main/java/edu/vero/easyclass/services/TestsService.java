59d85d3551c6dc30e6afe1a4fcbaf29f0b58d437
oufan123123 <34472175+oufan123123@users.noreply.github.com>
2017-12-17 22:17:31 +0800

    接口定义

---
+++ easyclass-service/src/main/java/edu/vero/easyclass/services/TestsService.java
@@ -0,0 +1,18 @@
+package edu.vero.easyclass.services;
+
+import edu.vero.easyclass.domain.OnlineClassTest;
+import edu.vero.easyclass.domain.Question;
+import edu.vero.easyclass.domain.TeacherArrangement;
+
+
+
+import java.util.List;
+
+public interface TestsService {
+    public List<OnlineClassTest> findAllTests();
+    public OnlineClassTest createTest(OnlineClassTest onlineClassTest);
+    public OnlineClassTest updateTest(OnlineClassTest onlineClassTest);
+    public OnlineClassTest findTest(Integer testId);
+    public TeacherArrangement findTestArrangement(Integer testId);
+    public List<Question> findTestQuestions(Integer testId);
+}
