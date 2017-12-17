73da75eeae33cded75f9189ca945f9441bcaefad
XiangDe <qq313700046@icloud.com>
2017-12-17 23:22:27 +0800

    修复student service 接口实现错误

--- easyclass-service/src/main/java/edu/vero/easyclass/services/impl/StudentServiceImpl.java
+++ easyclass-service/src/main/java/edu/vero/easyclass/services/impl/StudentServiceImpl.java
@@ -44,4 +44,9 @@ public class StudentServiceImpl implements StudentService {
     public Student save(Student student) {
         return studentJpaDao.save(student);
     }
+
+    @Override
+    public List<Student> findAll() {
+        return null;
+    }
 }
