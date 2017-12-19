d7a9eba5fc628aea3c78e67dd9440d208bdca9e0
LuletterSoul <qq313700046@icloud.com>
2017-12-19 13:25:43 +0800

    同

--- easyclass-repository/src/main/java/edu/vero/easyclass/repositories/ClassTimeJpaDao.java
+++ easyclass-repository/src/main/java/edu/vero/easyclass/repositories/ClassTimeJpaDao.java
@@ -1,8 +1,12 @@
 package edu.vero.easyclass.repositories;
 
+
 import edu.vero.easyclass.domain.ClassTime;
+import edu.vero.easyclass.domain.TeachingComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClassTimeJpaDao extends JpaRepository<ClassTime,Integer> {
 
-        }
+public interface ClassTimeJpaDao extends JpaRepository<ClassTime, Integer>
+{
+
+}
